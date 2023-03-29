f942702a283f8f6a49e4b2b6aa575aded9d9c502
lukesd456 <dariof_0504@hotmail.com>
2023-03-28 22:20:19 -0500

    Update Home.jsx

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -1,15 +1,7 @@
-import { useReducer } from "react"
-
 const Home = () => {
-  const [state, dispatch] = useReducer((state, action)=>{
-    state.obj1='lol'
-    state.obj2 = action
-    console.log(state)
-  }, {})
   return (
     <div>
       Home
-      <button onClick={()=>dispatch('lol')} >Cambiar</button>
     </div>
   )
 }
