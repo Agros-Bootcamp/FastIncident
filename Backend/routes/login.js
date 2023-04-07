c5abb267293625c9a8a7d4e800e82d5ad768a983
Cristhian Aldair Perez Roncal <cris71l124@hotmail.com>
2023-04-06 23:25:17 -0500

    Fusión de ramas actual

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -3,6 +3,7 @@ import { authTokens, refreshToken } from '../controllers/loginController.js'
 import { verifyRefreshJWT } from "../middleware/verifyJWT.js";
 
 export const routerLogin = Router()
+export const routerJWT = Router()
 
 
 routerLogin.post('/login', authTokens)
