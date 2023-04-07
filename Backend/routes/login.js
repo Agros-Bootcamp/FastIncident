3f36fa4a27267f9c93331201603c0f3ff69dfeb4
Cristhian Aldair Perez Roncal <cris71l124@hotmail.com>
2023-04-06 23:02:43 -0500

    VerifyJWT

--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authTokens, refreshToken } from '../controllers/loginController.js'
+import { verifyRefreshJWT } from '../middleware/verifyJWT.js';
 
 export const routerLogin = Router()
 export const routerJWT = Router()
 
 routerLogin.post('/login', authTokens)
-    .post('/refreshToken', refreshToken)
+    .post('/refreshToken', verifyRefreshJWT, refreshToken)
 
 
