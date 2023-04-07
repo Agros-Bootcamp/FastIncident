b568f5f82295a04d0d568fb82808e6619efc6e61
lukesd456 <dariof_0504@hotmail.com>
2023-04-06 22:32:11 -0500

    cambio backend integrante

--- frontend/src/pages/private/Homepage.jsx
+++ frontend/src/pages/private/Homepage.jsx
@@ -1,6 +1,21 @@
+import {useDispatch, useSelector} from 'react-redux'
+import { logOut } from '../../api/authSlice'
+
 const Homepage = () => {
+  const data = useSelector(state=>state.auth.UserInfo)
+  const dispatch = useDispatch()
+
+  const logoutSession = (e) => {
+    e.preventDefault()
+    dispatch(logOut())
+  }
+
   return (
-    <div>Homepage</div>
+    <div>
+      <h1>Bienvenido {data.first_name_user}</h1>
+      <h3>Tu rol es {data.title_rol_user}</h3>
+      <button onClick={e=>logoutSession(e)} >Cerrar Sesion</button>
+    </div>
   )
 }
 
